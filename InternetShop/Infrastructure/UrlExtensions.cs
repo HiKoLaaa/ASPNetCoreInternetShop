07b34196c8236304f2aeabed3ae1b99634de33c4
HiKoLaaa <hikolaaa2017@yandex.ru>
2019-09-22 01:02:53 +0300

    Добавил удобные переходы между страницами.

---
+++ InternetShop/Infrastructure/UrlExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace InternetShop.Infrastructure
+{
+	public static class UrlExtensions
+	{
+		public static string PathAndQuery(this HttpRequest request)
+		{
+			return request.QueryString.HasValue ? $"{request.Path}{request.QueryString}" : request.Path.ToString();
+		}
+	}
+}
